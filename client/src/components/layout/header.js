65c1f0a483435a371100f6d034e89b97430dab58
MeshalS <meshal.saus@gmail.com>
2021-01-15 06:04:53 +0300

    adding the sgin-up with nav

--- client/src/components/layout/header.js
+++ client/src/components/layout/header.js
@@ -1,27 +1,52 @@
 
 // or less ideally
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
+import useAuth from '../../hooks/auth';
+
+// import useAuth from '../../hooks/auth';
+
+// meshal=()=>{}
+//(elemet)=>{retuern elemet*2}
+// either above for export 
+const Header = () => {
+    const { isLoggedIn, logout, getProfile } = useAuth();
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+
+                    <Link to="/">Home</Link>
+                    {isLoggedIn() ?
+                        <>
+                        
+                            <Link to="/schools">school</Link>
+                            <Link to="/students">student</Link>
+                            <Link to="/teacher">teacher</Link>
+
+                            <h3> Hello, {getProfile().email}   </h3>
+                            <Link onClick={() => logout()} to='/'>Logout</Link>
+                        </>
+                        :
+                        <>
+                            <Link to="/signup">Signup</Link>
+                            <Link to="/login">Login</Link>
+                        </>
+
+                    }
 
 
-export default class header extends Component {
-    render() {
-        return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/schools">school</Nav.Link>
-                        <Nav.Link href="/students">student</Nav.Link>
-                        <Nav.Link href="/teacher">teacher</Nav.Link>
+                </Nav>
 
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+export default Header;
 
-                    </Nav>
-
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
-}
